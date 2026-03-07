d1699b06d2e7d170735a00e0211142a7465eef8e
Kirti <kirti19y@gmail.com>
2026-03-07 21:14:49 +0530

    preparing backend for deployment

--- server/app.js
+++ server/app.js
@@ -37,12 +37,16 @@ const errorHandler = require("./middleware/errorMiddleware");
 app.use(errorHandler);
 
 // Connect Database
+const PORT = process.env.PORT || 5000;
+
 mongoose.connect(process.env.MONGO_URI)
   .then(() => {
     console.log("MongoDB Connected");
-    app.listen(process.env.PORT || 5000, () => {
-      console.log("Server running on port 5000");
+
+    app.listen(PORT, () => {
+      console.log(`Server running on port ${PORT}`);
     });
+
   })
   .catch((err) => {
     console.error("Database connection error:", err);
